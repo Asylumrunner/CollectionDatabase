008aa59ee3f6776a7434756d79c8abdcd0dfab20
Michael Burdick <asylumrunner@gmail.com>
2023-11-18 16:40:06 -0800

    move common database functions to base class

--- Controllers/genre_controller.py
+++ Controllers/genre_controller.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 from fuzzywuzzy import process
 import boto3
 from .secrets import secrets
+from boto3.dynamodb.conditions import Key
+import json
 
 class GenreController(ABC):
 
@@ -21,25 +23,93 @@ class GenreController(ABC):
     def put_key(self, key):
         pass
 
-    @abstractmethod
     def get_key(self, key):
-        pass
+        response = {'status': 'FAIL'}
+        try:
+            db_response = self.dynamodb.get_item(
+                Key={
+                    'guid': self.guid_prefix + key
+                }
+            )
+            if(db_response['Item']):
+                response['item'] = db_response['Item']
+                response['status'] = 'OK'
+        except Exception as e:
+            print("Exception while retrieving key {} from database: {}".format(key, e))
+            response['error_message'] = str(e)
+        return response
 
-    @abstractmethod
     def delete_key(self, key):
-        pass
+        response = {'status': 'FAIL'}
+        try:
+            db_response = self.dynamodb.delete_item(
+                Key={
+                    'guid': self.guid_prefix + key
+                },
+                ReturnValues='ALL_OLD'
+            )
+            if(db_response['Attributes']):
+                response['item'] = db_response['Attributes']
+                response['status'] = 'OK'
+        except Exception as e:
+            print("Exception while deleting key {} from database: {}".format(key, e))
+            response['error_message'] = str(e)
+        return response
 
-    @abstractmethod
     def get_table(self):
-        pass
+        response = {}
+        db_response = {}
+        try:
+            while not response or 'LastEvaluatedKey' in db_response:
+                if('LastEvaluatedKey' not in db_response): 
+                    db_response = self.dynamodb.scan(
+                        FilterExpression=Key('guid').begins_with(self.guid_prefix)
+                    )
+                else:
+                    db_response = self.dynamodb.scan(
+                        FilterExpression=Key('guid').begins_with(self.guid_prefix),
+                        ExclusiveStartKey=db_response['LastEvaluatedKey']
+                    )
+                if(db_response['Items']):
+                    response['Items'] = db_response['Items']
+        except Exception as e:
+            print("Exception while getting table from database: {}".format(e))
+            response['error_message'] = str(e)
+        return response
 
-    @abstractmethod
     def back_up_table(self):
-        pass
-    
-    @abstractmethod
+        response = {'status': 'FAIL', 'controller': type(self).__name__}
+        try:
+            table_contents = self.get_table()
+            if('error_message' not in table_contents and 'Items' in table_contents):
+                guids = {'guids': [item['original_guid'] for item in table_contents['Items']]}
+                s3_response = self.s3.put(Body=bytes(json.dumps(guids).encode('UTF-8')))
+                response['object'] = s3_response
+                response['status'] = 'OK'
+            elif('error_message' in table_contents):
+                response['error_message'] = table_contents['error_message']
+            else:
+                response['error_message'] = "No table items to back up"
+        except Exception as e:
+            print("Exception while backing up table from database: {}".format(e))
+            response['error_message'] = str(e)
+        return response
+
     def restore_table(self):
-        pass
+        response = {'status': 'FAIL', 'controller': type(self).__name__}
+        try:
+            s3_response = self.s3.get()
+            s3_response_body = json.loads(s3_response['Body'].read().decode("utf-8"))
+            for guid in s3_response_body['guids']:
+                self.put_key(guid)
+            response['object'] = s3_response_body
+            response['status'] = 'OK'
+        except Exception as e:
+            print('Exception while restoring table from database: {}'.format(e))
+            print['error_message'] = str(e)
+        return response
+
+
 """
     @abstractmethod
     def wipe_table(self):
