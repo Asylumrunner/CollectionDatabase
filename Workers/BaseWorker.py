7e49b9774305f514e2c1bc909c5ae73244f6390e
Michael Burdick <asylumrunner@gmail.com>
2025-04-26 20:22:19 -0700

    Update BaseWorker.py

--- Workers/BaseWorker.py
+++ Workers/BaseWorker.py
@@ -11,10 +11,5 @@ class BaseWorker(ABC):
     def __init__(self):
         self.dynamodb = boto3.resource('dynamodb', region_name='us-east-1', aws_access_key_id=secrets['ACCESS_KEY'], aws_secret_access_key=secrets['SECRET_KEY']).Table('CollectionTable')
         if BaseWorker.database is None:
-            try:
-                BaseWorker.database = mysql.connector.connect(host=secrets["database_endpoint"], user=secrets["database_username"], password=secrets["database_password"], database=secrets["collection_database_name"])
-            except Exception as error:
-                print("Exception when attempting to create MySQL database connection {}".format(error))
-            else:
-                print("MySQL database connection established")
+            BaseWorker.database = mysql.connector.connect(host=secrets["database_endpoint"], user=secrets["database_username"], password=secrets["database_password"], database=secrets["collection_database_name"], port=3306)
             
