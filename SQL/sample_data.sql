aaf36a151dcd2481d2295e59137aae69203c82ba
Michael Burdick <asylumrunner@gmail.com>
2025-05-20 16:06:22 -0700

    Update sample_data.sql

---
+++ SQL/sample_data.sql
@@ -0,0 +1,63 @@
+INSERT INTO items (id, title, media_type, release_year, date_added, created_by, img_link, original_api_id) VALUES (891, "Dune", 99, 1950, "2025-05-20 22:15:03", "Frank Herbert", "https://picsum.photos/200/300", 69420);
+INSERT INTO items (id, title, media_type, release_year, date_added, created_by, img_link, original_api_id) VALUES (892, "Neuromancer", 99, 1995, "2025-05-20 22:15:03", "William Gibson", "https://picsum.photos/200/300", 69420);
+INSERT INTO items (id, title, media_type, release_year, date_added, created_by, img_link, original_api_id) VALUES (893, "Blood Meridian", 99, 1980, "2025-05-20 22:15:03", "Cormac McCarthy", "https://picsum.photos/200/300", 69420);
+INSERT INTO items (id, title, media_type, release_year, date_added, created_by, img_link, original_api_id) VALUES (894, "Dune", 100, 2021, "2025-05-20 22:15:03", "Denis Villeneuve", "https://picsum.photos/200/300", 69420);
+INSERT INTO items (id, title, media_type, release_year, date_added, created_by, img_link, original_api_id) VALUES (895, "Challengers", 100, 2021, "2025-05-20 22:15:03", "Luca Guadagnino", "https://picsum.photos/200/300", 69420);
+INSERT INTO items (id, title, media_type, release_year, date_added, created_by, img_link, original_api_id) VALUES (896, "Anora", 100, 2024, "2025-05-20 22:15:03", "Sean Baker", "https://picsum.photos/200/300", 69420);
+INSERT INTO items (id, title, media_type, release_year, date_added, created_by, img_link, original_api_id) VALUES (897, "Dishonored", 101, 2021, "2025-05-20 22:15:03", "Arkane Studios", "https://picsum.photos/200/300", 69420);
+INSERT INTO items (id, title, media_type, release_year, date_added, created_by, img_link, original_api_id) VALUES (898, "Yakuza 0", 101, 2017, "2025-05-20 22:15:03", "Ryu Ga Gotoku Studios", "https://picsum.photos/200/300", 69420);
+INSERT INTO items (id, title, media_type, release_year, date_added, created_by, img_link, original_api_id) VALUES (899, "Clair Obscur: Expedition 33", 101, 2025, "2025-05-20 22:15:03", "Sandfall Interactive", "https://picsum.photos/200/300", 69420);
+INSERT INTO items (id, title, media_type, release_year, date_added, created_by, img_link, original_api_id) VALUES (900, "Secret Hitler", 102, 2010, "2025-05-20 22:15:03", "Cards Against Humanity", "https://picsum.photos/200/300", 69420);
+INSERT INTO items (id, title, media_type, release_year, date_added, created_by, img_link, original_api_id) VALUES (901, "Coup", 102, 2008, "2025-05-20 22:15:03", "Indie Board and Cards", "https://picsum.photos/200/300", 69420);
+INSERT INTO items (id, title, media_type, release_year, date_added, created_by, img_link, original_api_id) VALUES (902, "Root", 102, 2018, "2025-05-20 22:15:03", "Leder Games", "https://picsum.photos/200/300", 69420);
+INSERT INTO items (id, title, media_type, release_year, date_added, created_by, img_link, original_api_id) VALUES (903, "Apocalypse World", 103, 2011, "2025-05-20 22:15:03", "D. Vincent Baker", "https://picsum.photos/200/300", 69420);
+INSERT INTO items (id, title, media_type, release_year, date_added, created_by, img_link, original_api_id) VALUES (904, "The Burning Wheel", 103, 2012, "2025-05-20 22:15:03", "Luke Crane", "https://picsum.photos/200/300", 69420);
+INSERT INTO items (id, title, media_type, release_year, date_added, created_by, img_link, original_api_id) VALUES (905, "Delta Green", 103, 2009, "2025-05-20 22:15:03", "Arc Dream Publishing", "https://picsum.photos/200/300", 69420);
+INSERT INTO items (id, title, media_type, release_year, date_added, created_by, img_link, original_api_id) VALUES (906, "Cowboy Bebop", 104, 1995, "2025-05-20 22:15:03", "Shinichiro Watanabe", "https://picsum.photos/200/300", 69420);
+INSERT INTO items (id, title, media_type, release_year, date_added, created_by, img_link, original_api_id) VALUES (907, "Samurai Champloo", 104, 1999, "2025-05-20 22:15:03", "Shinichiro Watanabe", "https://picsum.photos/200/300", 69420);
+INSERT INTO items (id, title, media_type, release_year, date_added, created_by, img_link, original_api_id) VALUES (908, "Space Dandy", 104, 2008, "2025-05-20 22:15:03", "Shinichiro Watanabe", "https://picsum.photos/200/300", 69420);
+INSERT INTO items (id, title, media_type, release_year, date_added, created_by, img_link, original_api_id) VALUES (909, "All Born Screaming", 105, 2024, "2025-05-20 22:15:03", "St. Vincent", "https://picsum.photos/200/300", 69420);
+INSERT INTO items (id, title, media_type, release_year, date_added, created_by, img_link, original_api_id) VALUES (910, "The New Sound", 105, 2024, "2025-05-20 22:15:03", "Geordie Greep", "https://picsum.photos/200/300", 69420);
+INSERT INTO items (id, title, media_type, release_year, date_added, created_by, img_link, original_api_id) VALUES (911, "Mountainhead", 105, 2024, "2025-05-20 22:15:03", "Everything Everything", "https://picsum.photos/200/300", 69420);
+
+INSERT INTO albums (id, summary) VALUES (909, "St Vincent's newest album, also released in Spanish");
+INSERT INTO albums (id, summary) VALUES (910, "Geordie Greep's solo debut after fronting black midi");
+INSERT INTO albums (id, summary) VALUES (911, "The most recent release from Brit pop band Everything Everything");
+
+INSERT INTO anime (id, episodes, summary) VALUES (906, 24, "A jazz-infused anime classic following the down-and-out crew of bounty hunters aboard the Bebop");
+INSERT INTO anime (id, episodes, summary) VALUES (907, 19, "Lo-fi hip hop meets feudal Japan as two feuding samurai are roped into helping a waitress find a samurai that smells of sunflowers");
+INSERT INTO anime (id, episodes, summary) VALUES (908, 28, "A horny idiot and his galactic crew of fools quests across the universe to hunt aliens to get money to go to Space Hooters");
+
+INSERT INTO board_games (id, minimum_players, maximum_players, summary, duration) VALUES (900, 5, 10, "Play the parliament of Weimar Germany and try to not elect Hitler", 60);
+INSERT INTO board_games (id, minimum_players, maximum_players, summary, duration) VALUES (901, 4, 8, "Lie and scheme to be the last player at the table with cards", 20);
+INSERT INTO board_games (id, minimum_players, maximum_players, summary, duration) VALUES (902, 3, 4, "Attempt to claim the woods as your own in this asymmetrical tactical game", 180);
+
+INSERT INTO books (id, isbn, printing_year) VALUES (891, "111111111111", 2012);
+INSERT INTO books (id, isbn, printing_year) VALUES (892, "222222222222", 2009);
+INSERT INTO books (id, isbn, printing_year) VALUES (893, "333333333333", 2010);
+
+INSERT INTO movies (id, lang, summary, duration) VALUES (894, "EN", "The first part of the adaptation of Frank Herbert's sci-fi epic", 160);
+INSERT INTO movies (id, lang, summary, duration) VALUES (895, "EN", "Two lifelong friends and tennis pros are torn apart over their mutual attraction to a woman, churros", 120);
+INSERT INTO movies (id, lang, summary, duration) VALUES (896, "EN", "A New York stripper has her Cinderella story after her sudden marriage to the son of a Russian oligarch", 120);
+
+INSERT INTO rpgs (id, isbn, summary) VALUES (903, "444444444444", "It's the apocalypse, have fun, fuck, and die");
+INSERT INTO rpgs (id, isbn, summary) VALUES (904, "555555555555", "It's a fantasy world, be sad and kill things");
+INSERT INTO rpgs (id, isbn, summary) VALUES (905, "666666666666", "Oh god why does that guy have so many tentacles");
+
+INSERT INTO video_games (id, summary) VALUES (897, "Reclaim your stolen honor and the throne of Dunwall");
+INSERT INTO video_games (id, summary) VALUES (898, "A real estate and club manager simulator with a crime drama on the side");
+INSERT INTO video_games (id, summary) VALUES (899, "Hon hon hon, would you like ze cigarette with ze JRPG?");
+
+INSERT INTO video_game_platforms (platform_id, platform_name) VALUES (1, "PC");
+INSERT INTO video_game_platforms (platform_id, platform_name) VALUES (2, "Playstation 4");
+INSERT INTO video_game_platforms (platform_id, platform_name) VALUES (3, "Playstation 5");
+INSERT INTO video_game_platforms (platform_id, platform_name) VALUES (4, "Xbox 360");
+INSERT INTO video_game_platforms (platform_id, platform_name) VALUES (5, "Xbox Series");
+
+INSERT INTO game_platform_availabilities (game_id, platform_id) VALUES (897, 1);
+INSERT INTO game_platform_availabilities (game_id, platform_id) VALUES (897, 2);
+INSERT INTO game_platform_availabilities (game_id, platform_id) VALUES (897, 4);
+INSERT INTO game_platform_availabilities (game_id, platform_id) VALUES (898, 1);
+INSERT INTO game_platform_availabilities (game_id, platform_id) VALUES (898, 2);
+INSERT INTO game_platform_availabilities (game_id, platform_id) VALUES (899, 5);
+INSERT INTO game_platform_availabilities (game_id, platform_id) VALUES (899, 1);
