5eb895515acf2b847ffae90e99df17a9deaa21c5
Michael Burdick <asylumrunner@gmail.com>
2025-05-22 16:10:15 -0700

    get rid of mediatypeconverter

--- Workers/BaseWorker.py
+++ Workers/BaseWorker.py
@@ -2,16 +2,13 @@ from abc import ABC, abstractmethod
 import boto3
 from .secrets import secrets
 from boto3.dynamodb.conditions import Key
-from ..Utilities.MediaTypeConverter import MediaTypeConverter
 import mysql.connector
-import json
 
 class BaseWorker(ABC):
     database = None
 
     def __init__(self):
         self.dynamodb = boto3.resource('dynamodb', region_name='us-east-1', aws_access_key_id=secrets['ACCESS_KEY'], aws_secret_access_key=secrets['SECRET_KEY']).Table('CollectionTable')
-        self.mediaTypeConverter = MediaTypeConverter()
         if BaseWorker.database is None:
             BaseWorker.database = mysql.connector.connect(host=secrets["database_endpoint"], user=secrets["database_username"], password=secrets["database_password"], database=secrets["collection_database_name"], port=3306)
             
