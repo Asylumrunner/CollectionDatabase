e6fedb5a44d212168a2204a1a7bf49a283517463
Michael Burdick <asylumrunner@gmail.com>
2025-10-19 16:06:43 -0700

    start working on form validation with pydantic

--- Utilities/ValidateRequestInput.py
+++ Utilities/ValidateRequestInput.py
@@ -1,5 +1,77 @@
 import logging
-from wtforms import Form, StringField, IntegerField, validators
+from datetime import datetime
+from pydantic import BaseModel, PositiveInt
+
+class Album(BaseModel):
+    id: int
+    title: str
+    release_year: int
+    created_by: str
+    img_link: str
+    original_api_id: str
+    summary: str
+
+class Anime(BaseModel):
+    id: int
+    title: str
+    release_year: int
+    created_by: str
+    img_link: str
+    original_api_id: str
+    episodes: int
+
+class BoardGame(BaseModel):
+    id: int
+    title: str
+    release_year: int
+    created_by: str
+    img_link: str
+    original_api_id: str
+    minimum_players: int
+    maximum_players: int
+    summary: str
+    duration: int
+
+class Book(BaseModel):
+    id: int
+    title: str
+    release_year: int
+    created_by: str
+    img_link: str
+    original_api_id: str
+    isbn: int
+    printing_year: str
+
+class Movie(BaseModel):
+    id: int
+    title: str
+    release_year: int
+    created_by: str
+    img_link: str
+    original_api_id: str
+    lang: str
+    summary: str
+    duration: int
+
+class RPG(BaseModel):
+    id: int
+    title: str
+    release_year: int
+    created_by: str
+    img_link: str
+    original_api_id: str
+    isbn: int
+    summary: str
+
+class VideoGame(BaseModel):
+    id: int
+    title: str
+    release_year: int
+    created_by: str
+    img_link: str
+    original_api_id: str
+    summary: str
+
 
 def fail_because(reason_string):
     response = {
